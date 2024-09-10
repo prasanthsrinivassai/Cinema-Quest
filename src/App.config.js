f464585877c992be6af794b95eb1af9f755fbcef
prasanthsrinivassai <sprasanthsrinivassai@gmail.com>
2024-09-10 22:34:20 +0530

    api call added

--- src/App.config.js
+++ src/App.config.js
@@ -1,5 +1,4 @@
 export const CONFIG = {
-  API_KEY:
-    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlM2RiNWU1Y2YwYzY5ZmY1ZTBkNzkwNDhjNmE2NGFkZiIsIm5iZiI6MTcyNTU1NjY4My4yODYzMjYsInN1YiI6IjY2ZDllNzIwNzZiMTI5YjI2MWI4ZDg0NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.u4meQRJwS1Nx7f2jPv8tes9Q5LAg_Lj9JSbFm4crBwQ", // Replace with your actual API key
-  BASE_URL: "https://api.themoviedb.org/3/",
+    API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlM2RiNWU1Y2YwYzY5ZmY1ZTBkNzkwNDhjNmE2NGFkZiIsIm5iZiI6MTcyNTU1NjY4My4yODYzMjYsInN1YiI6IjY2ZDllNzIwNzZiMTI5YjI2MWI4ZDg0NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.u4meQRJwS1Nx7f2jPv8tes9Q5LAg_Lj9JSbFm4crBwQ", // Replace with your actual API key
+    BASE_URL: "https://api.themoviedb.org/3/",
 };
